55df50f23c6a8c3fcbd3a0d29da847fa3e055449
Andri Purnomo <andri.adrp@gmail.com>
2023-04-12 20:58:48 +0700

    accident counter

---
+++ apps/accident-counter/src/app/app.tsx
@@ -0,0 +1,12 @@
+import './app.css';
+import Counter from './counter';
+
+export function App() {
+  return (
+    <main className="flex items-center h-full place-content-center bg-cyan-900">
+      <Counter />
+    </main>
+  );
+}
+
+export default App;
